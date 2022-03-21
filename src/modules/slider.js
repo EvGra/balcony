ee68656a80c50d5f279723d46e32683cdfc3ec7f
Evgeny Grabun <grabun96@mail.ru>
2022-03-21 07:56:57 +0300

    выполнение слайдера для блока "наши услуги"

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,42 +1,70 @@
 export const slider = () => {
-  const sliderBlock = document.querySelector('.benefits-wrap')
-  const slides = document.querySelectorAll('.benefits__item')
+  const sliderBlock = document.querySelector('.services')
+  const slides = sliderBlock.querySelectorAll('.col-md-12')
  
-
   let currentSlide = 0
+  let seccondCurrentSlide = 1
 
-
-  const prevSlide = (elems, index, strClass) => {
-    elems[index].classList.remove(strClass)
+  const prevSlide = (elems, index) => {
+    elems[index].style = 'display: none'
   }
 
-  const nextSlide = (elems, index, strClass) => {
-    elems[index].classList.add(strClass)
+  const nextSlide = (elems, index) => {
+    elems[index].style = 'display: flex'
   }
 
+    sliderBlock.addEventListener('click', (e) => {
+      e.preventDefault()
 
-  sliderBlock.addEventListener('click', (e) => {
-    e.preventDefault()
+      if (document.documentElement.offsetWidth <= 576) {
+        prevSlide(slides, currentSlide)
 
-    if(!e.target.matches('.benefits__arrow')) {
-      return
-    }
-    prevSlide(slides, currentSlide, 'benefits__item-active')
+        if (e.target.closest('#arrow-right')) {
+          currentSlide++
+        } else if (e.target.closest('#arrow-left')) {
+          currentSlide--
+        }
 
-    if (e.target.matches('#arrow-right')) {
-      currentSlide++
-    } else if (e.target.matches('#arrow-left')) {
-      currentSlide--
-    }
+        if (currentSlide >= slides.length) {
+          currentSlide = 0
+        }
 
-    if (currentSlide >= slides.length) {
-      currentSlide = 0
-    }
+        if (currentSlide < 0) {
+          currentSlide = slides.length - 1
+        }
 
-    if (currentSlide < 0) {
-      currentSlide = slides.length - 1
-    }
+        nextSlide(slides, currentSlide)
+      } else {
+        prevSlide(slides, currentSlide)
+        prevSlide(slides, seccondCurrentSlide)
 
-    nextSlide(slides, currentSlide, 'benefits__item-active')
-  })
+        if (e.target.closest('#arrow-right')) {
+          currentSlide = currentSlide + 2
+          seccondCurrentSlide = seccondCurrentSlide + 2
+        } else if (e.target.closest('#arrow-left')) {
+          currentSlide = currentSlide - 2
+          seccondCurrentSlide = seccondCurrentSlide - 2
+        }
+
+        if (currentSlide >= slides.length) {
+          currentSlide = 0
+        }
+
+        if (seccondCurrentSlide >= slides.length) {
+          seccondCurrentSlide = 1
+        }
+
+        if (currentSlide < 0) {
+          currentSlide = slides.length - 2
+        }
+
+        if (seccondCurrentSlide < 0) {
+          seccondCurrentSlide = slides.length - 1
+        }
+
+        nextSlide(slides, currentSlide)
+        nextSlide(slides, seccondCurrentSlide)
+      }
+    })
+  
 }
