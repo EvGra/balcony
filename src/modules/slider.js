b86be3bf7ffa8df4053f49b6d9f7facdd6466520
Evgeny Grabun <grabun96@mail.ru>
2022-03-21 16:05:10 +0300

    добавление слайдера для "выгоды для клиентов"

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,9 +1,15 @@
 export const slider = () => {
   const sliderBlock = document.querySelector('.services')
   const slides = sliderBlock.querySelectorAll('.col-md-12')
- 
+  const benefits = document.querySelector('#benefits')
+  const benefitsItems = benefits.querySelectorAll('.benefits__item')
+
   let currentSlide = 0
   let seccondCurrentSlide = 1
+
+  let currentBenefits = 0
+  let seccondBenefits = 1
+  let thirdBenefits = 2
 
   const prevSlide = (elems, index) => {
     elems[index].style = 'display: none'
@@ -13,58 +19,128 @@ export const slider = () => {
     elems[index].style = 'display: flex'
   }
 
-    sliderBlock.addEventListener('click', (e) => {
-      e.preventDefault()
+  const nextSlideBenefits = (elems, index) => {
+    elems[index].style = 'display: block'
+  }
 
-      if (document.documentElement.offsetWidth <= 576) {
-        prevSlide(slides, currentSlide)
+  sliderBlock.addEventListener('click', (e) => {
 
-        if (e.target.closest('#arrow-right')) {
-          currentSlide++
-        } else if (e.target.closest('#arrow-left')) {
-          currentSlide--
-        }
+    e.preventDefault()
 
-        if (currentSlide >= slides.length) {
-          currentSlide = 0
-        }
+    if (document.documentElement.offsetWidth <= 576) {
+      prevSlide(slides, currentSlide)
 
-        if (currentSlide < 0) {
-          currentSlide = slides.length - 1
-        }
-
-        nextSlide(slides, currentSlide)
-      } else {
-        prevSlide(slides, currentSlide)
-        prevSlide(slides, seccondCurrentSlide)
-
-        if (e.target.closest('#arrow-right')) {
-          currentSlide = currentSlide + 2
-          seccondCurrentSlide = seccondCurrentSlide + 2
-        } else if (e.target.closest('#arrow-left')) {
-          currentSlide = currentSlide - 2
-          seccondCurrentSlide = seccondCurrentSlide - 2
-        }
-
-        if (currentSlide >= slides.length) {
-          currentSlide = 0
-        }
-
-        if (seccondCurrentSlide >= slides.length) {
-          seccondCurrentSlide = 1
-        }
-
-        if (currentSlide < 0) {
-          currentSlide = slides.length - 2
-        }
-
-        if (seccondCurrentSlide < 0) {
-          seccondCurrentSlide = slides.length - 1
-        }
-
-        nextSlide(slides, currentSlide)
-        nextSlide(slides, seccondCurrentSlide)
+      if (e.target.closest('#arrow-right')) {
+        currentSlide++
+      } else if (e.target.closest('#arrow-left')) {
+        currentSlide--
       }
-    })
-  
+
+      if (currentSlide >= slides.length) {
+        currentSlide = 0
+      }
+
+      if (currentSlide < 0) {
+        currentSlide = slides.length - 1
+      }
+
+      nextSlide(slides, currentSlide)
+    } else {
+      prevSlide(slides, currentSlide)
+      prevSlide(slides, seccondCurrentSlide)
+
+      if (e.target.closest('#arrow-right')) {
+        currentSlide = currentSlide + 2
+        seccondCurrentSlide = seccondCurrentSlide + 2
+      } else if (e.target.closest('#arrow-left')) {
+        currentSlide = currentSlide - 2
+        seccondCurrentSlide = seccondCurrentSlide - 2
+      }
+
+      if (currentSlide >= slides.length) {
+        currentSlide = 0
+      }
+
+      if (seccondCurrentSlide >= slides.length) {
+        seccondCurrentSlide = 1
+      }
+
+      if (currentSlide < 0) {
+        currentSlide = slides.length - 2
+      }
+
+      if (seccondCurrentSlide < 0) {
+        seccondCurrentSlide = slides.length - 1
+      }
+
+      nextSlide(slides, currentSlide)
+      nextSlide(slides, seccondCurrentSlide)
+    }
+  })
+  benefits.addEventListener('click', (e) => {
+    e.preventDefault()
+
+    if (document.documentElement.offsetWidth <= 576) {
+      prevSlide(benefitsItems, currentBenefits)
+
+      if (e.target.closest('.benefits__arrow--right')) {
+        currentBenefits++
+      } else if (e.target.closest('.benefits__arrow--left')) {
+        currentBenefits--
+      }
+
+      if (currentBenefits >= benefitsItems.length) {
+        currentBenefits = 0
+      }
+
+      if (currentBenefits < 0) {
+        currentBenefits = benefitsItems.length - 1
+      }
+
+      nextSlideBenefits(benefitsItems, currentBenefits)
+    } else {
+      prevSlide(benefitsItems, currentBenefits)
+      prevSlide(benefitsItems, seccondBenefits)
+      prevSlide(benefitsItems, thirdBenefits)
+
+      if (e.target.closest('.benefits__arrow--right')) {
+        currentBenefits = currentBenefits + 3
+        seccondBenefits = seccondBenefits + 3
+        thirdBenefits = thirdBenefits + 3
+
+      } else if (e.target.closest('.benefits__arrow--left')) {
+        currentBenefits = currentBenefits - 3
+        seccondBenefits = seccondBenefits - 3
+        thirdBenefits = thirdBenefits - 3
+      }
+
+      if (currentBenefits >= benefitsItems.length) {
+        currentBenefits = 0
+      }
+
+      if (seccondBenefits >= benefitsItems.length) {
+        seccondBenefits = 1
+      }
+
+      if (thirdBenefits >= benefitsItems.length) {
+        thirdBenefits = 2
+      }
+
+      if (currentBenefits < 0) {
+        currentBenefits = benefitsItems.length - 3
+      }
+
+      if (seccondBenefits < 0) {
+        seccondBenefits = benefitsItems.length - 2
+      }
+
+      if (thirdBenefits < 0) {
+        thirdBenefits = benefitsItems.length - 1
+      }
+
+      nextSlideBenefits(benefitsItems, currentBenefits)
+      nextSlideBenefits(benefitsItems, seccondBenefits)
+      nextSlideBenefits(benefitsItems, thirdBenefits)
+    }
+  })
 }
